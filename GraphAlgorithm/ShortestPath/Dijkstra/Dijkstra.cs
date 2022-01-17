90daa22322256b8f709a9f79bfffbf19bb998e40
BrzValentin <brzvalentin1997@gmail.com>
2022-01-18 01:45:21 +0200

    added dijkstra alg

--- GraphAlgorithm/ShortestPath/Dijkstra/Dijkstra.cs
+++ GraphAlgorithm/ShortestPath/Dijkstra/Dijkstra.cs
@@ -8,6 +8,55 @@ public class Dijkstra : IAlgorithm
     
     public void Run(IMatrixGraph graph)
     {
-        
+        var n = graph.Size;
+        var distance = new int [n];
+        var parents = new int [n];
+        var visited = new bool [n];
+
+        PreInitialization(distance, parents);
+
+        for (int i = 0; i < n; i++)
+        {
+            int index = LastNotVisitedMinimum(distance, visited);
+            visited[index] = true;
+
+            for (int j = 0; j < n; j++)
+            {
+                var posible = distance[index] + graph.Matrix[index, j];
+                if (!visited[j] && graph.Matrix[index, j] != int.MaxValue && posible < distance[j])
+                {
+                    distance[j] = posible;
+                    parents[j] = index;
+                }
+            }
+        }
+    }
+
+    private int LastNotVisitedMinimum(int[] distance, bool[] visited)
+    {
+        int minimum = int.MaxValue;
+        int minimumIndex = -1;
+
+        for (int i = 0; i < distance.Length; i++)
+            if (!visited[i] && distance[i] <= minimum)
+            {
+                minimumIndex = i;
+                minimum = distance[i];
+            }
+
+        return minimumIndex;
+    }
+
+    private void PreInitialization(int[] distance, int[] parents)
+    {
+        for (int i = 1; i < distance.Length; i++)
+        {
+            distance[i] = int.MaxValue;
+        }
+
+        for (int i = 0; i < parents.Length; i++)
+        {
+            parents[i] = -1;
+        }
     }
 }
--- Program/Program.cs
+++ Program/Program.cs
@@ -3,8 +3,13 @@
 using Graph;
 using Graph.Factory;
 using Graph.Parser;
+using GraphAlgorithm;
+using GraphAlgorithm.ShortestPath.Dijkstra;
 
 var parser = new VertexAdjacencyMatrixWithInfinityGraphParser($"{Utils.RelativePathToResFile}\\VertexAdjacencyMatrixGraphWithInfinity.txt");
 var factory = new VertexAdjacencyMatrixGraphFactory(parser);
 var graph = factory.Create();
+
+IAlgorithm dijkstra = new Dijkstra();
+dijkstra.Run(graph);
 Console.WriteLine("Hello, World!");
